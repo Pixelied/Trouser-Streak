26d07e806bd089e2ec914a571548f0282c64ce46
Pixelied <aemouzon@gmail.com>
2026-08-08 19:31:40 +0200

    test: run every standalone core test main

--- tools/run-core-tests.sh
+++ tools/run-core-tests.sh
@@ -8,5 +8,7 @@ mkdir -p "$OUT"
   find "$ROOT/src/main/java/dev/hypershot/core" -name '*.java' -print0
   find "$ROOT/src/test/java/dev/hypershot/core" -name '*TestMain.java' -print0
 } | xargs -0 javac --release 21 -d "$OUT"
-java -cp "$OUT" dev.hypershot.core.CoreTestMain
-java -cp "$OUT" dev.hypershot.core.CameraCoreTestMain
+for test_source in "$ROOT"/src/test/java/dev/hypershot/core/*TestMain.java; do
+  test_class="$(basename "$test_source" .java)"
+  java -cp "$OUT" "dev.hypershot.core.$test_class"
+done
